6da68189148e4f4c88f83aba850a5a7361a66136
fionayuqizhang <yu.qi.zhang@ocsbstudent.ca>
2021-01-08 14:20:06 +0000

    i

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-let date = ""
-let age = ""
+let date = ''
+let age = ''
+document.getElementById("submit").addEventListener('click', number)
 function number() {
-date = document.getElementById("date").value
-age = document.getElementById("age").value
-if((date == "Monday"||date == "Tuesday"||date == "Wednesday"||date == "Thursday"||date == "Friday")&&(age>2 && age<21)){
-  alert("Time for school!");
+date = document.getElementById('date').value
+age = document.getElementById('age').value
+if ((date === 'Monday'| |date === 'Tuesday'| |date === 'Wednesday'| |date === 'Thursday'| |date === 'Friday')&&(age > 2 & & age < 21)){
+  alert('Time for school!');
 }
-else if((date == "Saturday"||date == "Sunday")){
-  alert("No work today!")
+else if ((date === 'Saturday'| |date === 'Sunday')){
+  alert('No work today!')
 }
-else{
-  alert("Time for work!")
+else {
+  alert('Time for work!')
 }
 }
