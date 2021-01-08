14274fb2ec406dffa0bca257f03193d55555b0c3
fionayuqizhang <yu.qi.zhang@ocsbstudent.ca>
2021-01-08 03:08:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let date = ""
+let age = ""
+function number() {
+date = document.getElementById("date").value
+age = document.getElementById("age").value
+if((date == "Monday"||date == "Tuesday"||date == "Wednesday"||date == "Thursday"||date == "Friday")&&(age>2 && age<21)){
+  alert("Time for school!");
+}
+else if((date == "Saturday"||date == "Sunday")){
+  alert("No work today!")
+}
+else{
+  alert("Time for work!")
+}
+}
